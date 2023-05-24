d1d9ec835f73b7caca20fc8477a4bdf5fb168f02
Ash Bridges <ashbridgescodes@gmail.com>
2023-05-24 17:06:14 +0100

    removed metadata from client component

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,6 @@ import './globals.css'
 import DataLayer from './context/DataLayer'
 import { ThemeProvider } from 'next-themes';
 
-export const metadata = {
-  title: 'NextJS Calendar',
-  description: 'Simple calendar app built with NextJS',
-}
-
 export default function RootLayout({ children }) {
 
   return (
